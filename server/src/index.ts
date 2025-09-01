68cb34cb68ba5e197337c0111f8490254779bd52
Exe16Kishan <kishanshukla53@gmail.com>
2025-09-01 22:23:54 +0530

    setup server side

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,25 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import * as trpcExpress from '@trpc/server/adapters/express';
+import { appRouter } from './router';
+import { createContext } from './trpc';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, TypeScript Express!');
-});
+
+app.use(
+  '/trpc',
+  trpcExpress.createExpressMiddleware({
+    router: appRouter ,
+    createContext,
+  }),
+);
+
+// app.get('/', (req: Request, res: Response) => {
+//   res.send('Hello, TypeScript Express!');
+// });
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
